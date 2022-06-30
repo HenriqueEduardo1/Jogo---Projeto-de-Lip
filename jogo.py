f5f9dc9bffdd267806a288aad8f584435e90c2f8
HenriqueEduardo1 <henriqueeduardo1123@gmail.com>
2022-06-30 18:41:35 -0300

    criação do módulo game_functions e ajustes no código

--- jogo.py
+++ jogo.py
@@ -1,44 +1,30 @@
-import sys
 import os
 import pygame
 from pygame.locals import *
 from settings import Settings
 from morcego import Morcego
 from fundo import Fundo
+import game_functions as gf
+
 
 def run_game():
     pygame.init()
     ai_settings = Settings()
-    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    screen = pygame.display.set_mode((ai_settings.screen_w, ai_settings.screen_h))
     pygame.display.set_caption("El Morcegón")
 
-    bat = Morcego()
     todas_as_sprites = pygame.sprite.Group()
     sprite_fundo = pygame.sprite.Group()
+
+    bat = Morcego(screen)
     todas_as_sprites.add(bat)
 
     for i in range(2):
-        fundo = Fundo(640,480,640 * i)
+        fundo = Fundo(ai_settings.screen_w, ai_settings.screen_h, ai_settings.screen_w * i)
         sprite_fundo.add(fundo)
 
-
     while True:
-        for event in pygame.event.get(): 
-                if event.type == pygame.QUIT:
-                    sys.exit()
-        if sprite_fora_da_tela(sprite_fundo.sprites()[0]):
-            sprite_fundo.remove(sprite_fundo.sprites()[0])
-            novo_fundo = Fundo(640,480,640 * i-1)
-            sprite_fundo.add(novo_fundo)
-
-        sprite_fundo.draw(screen)
-        todas_as_sprites.draw(screen)
-        todas_as_sprites.update()
-        sprite_fundo.update()
-        pygame.display.flip()
-
-def sprite_fora_da_tela(sprite):
-    return sprite.rect[0] < -(sprite.rect[2])
-
+        gf.check_events()
+        gf.update_screen(ai_settings, screen, sprite_fundo, todas_as_sprites)
 
 run_game()
